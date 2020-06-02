30c745ea47847630897ff7578c797a741a850936
GitKhab <gitkhab@yandex.ru>
2020-06-03 00:02:30 +0300

    Add lazyRequireTask function

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,17 @@
 'use strict';
 
 const gulp = require('gulp');
+
+
+// =============================================================================
+// функция "ленивой" загрузки модуля
+// =============================================================================
+
+function lazyRequireTask(taskName, path, options) {
+  options.taskName = taskName;
+  options = options || {};
+  gulp.task(taskName, function(callback) {
+    let task = require(path).call(this, options);
+    return task(callback);
+  });
+}
