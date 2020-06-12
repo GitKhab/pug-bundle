4f8063c841730bfd7ad4efb0b1348425e57ecc54
GitKhab <gitkhab@yandex.ru>
2020-06-12 22:27:46 +0300

    Remove postcss from css build task

--- gulp-tasks/build/css.js
+++ gulp-tasks/build/css.js
@@ -5,23 +5,10 @@ const sourcemaps = require('gulp-sourcemaps');
 const cache = require('gulp-cached');
 const gulpIf = require('gulp-if');
 const argv = require('yargs').argv;
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
 const remember = require('gulp-remember');
 const concat = require('gulp-concat');
 
 module.exports = function(options) {
-
-  const postcssPreset = {
-    dev: postcss([
-      autoprefixer()
-    ]),
-    prod: postcss([
-      autoprefixer(),
-      cssnano()
-    ])
-  };
 
   const concatPreset = {
     dev: concat('styles.css'),
@@ -32,7 +19,6 @@ module.exports = function(options) {
     return gulp.src(options.src, {base: options.base})
         .pipe(sourcemaps.init())
         .pipe(cache('css'))
-        .pipe(gulpIf(argv.prod, postcssPreset.prod, postcssPreset.dev))
         .pipe(remember('css'))
         .pipe(gulpIf(argv.prod, concatPreset.prod, concatPreset.dev))
         .pipe(sourcemaps.write())
