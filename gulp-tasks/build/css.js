799680098499c7b5b58483676df708a38530e573
GitKhab <gitkhab@yandex.ru>
2020-06-12 22:32:27 +0300

    Remove concat from css build task

--- gulp-tasks/build/css.js
+++ gulp-tasks/build/css.js
@@ -4,19 +4,12 @@ const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const gulpIf = require('gulp-if');
 const argv = require('yargs').argv;
-const concat = require('gulp-concat');
 
 module.exports = function(options) {
-
-  const concatPreset = {
-    dev: concat('styles.css'),
-    prod: concat('styles.css', {newLine: ''})
-  };
 
   return function() {
     return gulp.src(options.src, {base: options.base})
         .pipe(sourcemaps.init())
-        .pipe(gulpIf(argv.prod, concatPreset.prod, concatPreset.dev))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(options.dist));
   };
