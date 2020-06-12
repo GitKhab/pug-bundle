7ade87f352aa560a58476af1f385c019c4d5371d
GitKhab <gitkhab@yandex.ru>
2020-06-13 00:48:33 +0300

    Add stylus to css build task

--- gulp-tasks/build/css.js
+++ gulp-tasks/build/css.js
@@ -4,12 +4,19 @@ const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const gulpIf = require('gulp-if');
 const argv = require('yargs').argv;
+const stylus = require('gulp-stylus');
 
 module.exports = function(options) {
+
+  const stylusPreset = {
+    dev: stylus(),
+    prod: stylus({compress: true})
+  };
 
   return function() {
     return gulp.src(options.src, {base: options.base})
         .pipe(sourcemaps.init())
+        .pipe(gulpIf(argv.prod, stylusPreset.prod, stylusPreset.dev))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(options.dist));
   };
